d60aaa63c6ca6aaa61d8564192c1bd638a9b5457
ggustavo19 <gacarmona51@gmail.com>
2024-03-21 20:50:58 -0500

    Final

---
+++ crowdfunding_db.sql
@@ -0,0 +1,17 @@
+-- SQL Queries to verify that csv info uploaded to tables 
+
+--checking category table
+SELECT *
+FROM category
+
+--checking subcategory table
+SELECT *
+FROM subcategory
+
+--checking contacts table
+SELECT *
+FROM contacts
+
+--checking campaign table
+SELECT *
+FROM campaign
